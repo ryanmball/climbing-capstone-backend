74171ed4eb346e066a28496649fbdb19c3ebb29a
ryan <ball.ryan.rb@gmail.com>
2021-06-15 14:55:06 -0600

    adds seed data for routes, records, collections and images - associations are working properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+# ROUTE SEEDING
+# CSV.foreach(".csv") do |row|
+#   Route.create(
+#     name: row[0],
+#     location: row[1],
+#     mp_url: row[2],
+#     grade: row[3],
+#     lat: row[4],
+#     lon: row[5],
+#   )
+# end
+
+# RECORD SEEDING
+# CSV.foreach(".csv") do |row|
+#   Record.create(
+#     date: row[0],
+#     route_id: row[1],
+#     grade: row[2],
+#     result: row[3],
+#     in_progress: row[4],
+#     rating: row[5],
+#     partner: row[6],
+#     comments: row[7],
+#     collection_id: row[8],
+#     user_id: row[9],
+#   )
+# end
+
+Collection.create(
+  name: "Rifle",
+  partners: "Mariah, Dylan, Dillon, Will, Beth, Kathleen, Mark",
+  highlights: "Rifle was freaking awesome. Climbing summer camp for adults is a very fitting name. So much fun to walk between crags and the vibe was just fantastic. To top it off, every route I tried was unique and SO FUN. Can't wait to get back for Ricochet, Family Jewels and Baby Jesus",
+  user_id: 1,
+)
