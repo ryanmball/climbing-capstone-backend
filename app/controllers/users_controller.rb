d80ac9f671d0b17277d5df1e0b8d68d207122ebf
ryan <ball.ryan.rb@gmail.com>
2021-06-18 13:23:56 -0600

    adds users index, show, update and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, except: [:index, :create]
+
+  def index
+    render json: User.all.order(:id)
+  end
+
   def create
     user = User.new(
       first_name: params[:first_name],
@@ -12,5 +18,29 @@ class UsersController < ApplicationController
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
+  end
+
+  def show
+    render json: User.find(current_user.id)
+  end
+  
+  def update
+    user = User.find(current_user.id)
+    user.first_name = params[:first_name] || user.first_name
+    user.last_name = params[:last_name] || user.last_name
+    user.email = params[:email] || user.email
+    # user.password = params[:password] || user.password
+    # NEED TO FIGURE OUT HOW TO UPDATE PASSWORD
+    if user.save
+      render json: { message: "User updated successfully" }
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(current_user.id)
+    user.destroy
+    render json: { message: "User successfully destroyed!" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get "/users" => "users#index"
   post "/users" => "users#create"
+  get "/users/:id" => "users#show"
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
 
   post "/sessions" => "sessions#create"
 
