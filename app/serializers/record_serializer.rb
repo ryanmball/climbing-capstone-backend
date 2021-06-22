e387e25894b13da74a72aa4e93978cd9ad5688cc
ryan <ball.ryan.rb@gmail.com>
2021-06-22 15:15:32 -0600

    udpates record serializer

--- app/serializers/record_serializer.rb
+++ app/serializers/record_serializer.rb
@@ -1,7 +1,7 @@
 class RecordSerializer < ActiveModel::Serializer
-  attributes :id, :date, :grade, :result, :in_progress, :rating, :partner, :comments, :user_id
+  attributes :id, :date, :grade, :result, :in_progress, :rating, :partner, :comments, :user_id, :route
 
-  belongs_to :route
+  # belongs_to :route
   belongs_to :collection
   belongs_to :user
 end
