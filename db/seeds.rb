22a37a6a2944e13c8fe964a5a621e3d6c515c55b
ryan <ball.ryan.rb@gmail.com>
2022-09-09 11:16:23 -0600

    adds route_seed_file_15

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require "csv"
 
 # ROUTE SEEDING
-CSV.foreach("route_seed_file_14.csv") do |row|
+CSV.foreach("route_seed_file_15.csv") do |row|
   Route.create(
     name: row[0],
     location: row[1],
